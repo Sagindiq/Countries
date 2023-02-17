147b921b13d4b46e46fcf979d4cd8bbbced3e86e
Sagindiq <sogindiq77@gmail.com>
2023-02-17 23:08:39 +0500

    MUI theme added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import { createTheme, CssBaseline, PaletteMode, Theme, ThemeProvider } from "@mui/material";
 
 function App() {
+  const palette = localStorage.getItem("paletType");
+  const paletType: PaletteMode = palette == "dark" ? "dark" : "light";
+
+  const theme: Theme = createTheme({
+    palette: {
+      mode: paletType,
+      action: {
+        hoverOpacity: 0.08,
+        disabledOpacity: 0.26,
+        disabledBackground: "rgba(0, 0, 0, 0.12)",
+        disabled: "rgba(0, 0, 0, 0.26)",
+      },
+    },
+  });
+
   return (
-    <div className="App">
-    </div>
+    <ThemeProvider theme={theme}>
+      <h1>HELLO WORLD</h1>
+      <CssBaseline />
+    </ThemeProvider>
   );
 }
 
